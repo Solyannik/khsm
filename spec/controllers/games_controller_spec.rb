0a1898ad47b99bcc84e0a4a8727e2636f5f42fb5
Solyannik <solyannikolga@gmail.com>
2021-04-19 23:51:52 +0200

    add test "show alien game"

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -61,7 +61,18 @@ RSpec.describe GamesController, type: :controller do
       expect(response.status).to eq(200) # должен быть ответ HTTP 200
       expect(response).to render_template('show') # и отрендерить шаблон show
     end
+    
+    it "#show alien game" do
+      alien_game = FactoryBot.create(:game_with_questions)
 
+      get :show, id: alien_game.id
+      game = assigns(:game)
+
+      expect(response.status).not_to eq(200)
+      expect(response).to redirect_to(root_path)
+      expect(flash[:alert]).to be
+    end
+    
     # юзер отвечает на игру корректно - игра продолжается
     it 'answers correct' do
       # передаем параметр params[:letter]
