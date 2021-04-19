c548692095a53edcd33722af4bb106fb14179725
Solyannik <solyannikolga@gmail.com>
2021-04-19 23:59:10 +0200

    add test "try to create second game"

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -50,7 +50,19 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game))
       expect(flash[:notice]).to be
     end
+    
+    it 'try to create second game' do
+      expect(game_w_questions.finished?).to be_falsey
 
+      expect { post :create }.to change(Game, :count).by(0)
+
+      game = assigns(:game)
+      expect(game).to be nil
+
+      expect(response).to redirect_to(game_path(game_w_questions))
+      expect(flash[:alert]).to be
+    end
+    
     # юзер видит свою игру
     it '#show game' do
       get :show, id: game_w_questions.id
@@ -111,7 +123,7 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to user_path(user)
       expect(flash[:warning]).to be
     end
-    
+
     # тест на отработку "помощи зала"
     it 'uses audience help' do
       # сперва проверяем что в подсказках текущего вопроса пусто
